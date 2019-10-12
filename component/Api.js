d425c0f14bfca231f5786d8debf1276e27fb9d2e
nguyenminhcong <nguyenminhcong.27021997@gmail.com>
2019-10-12 23:15:57 +0700

    hehe

---
+++ component/Api.js
@@ -0,0 +1,11 @@
+const UrlBase = "http://192.168.1.113/" + "DocTruyen/";
+
+const DangNhapService = UrlBase + "DangNhap.php";
+const GetToanBoTheLoaiService = UrlBase + "GetToanBoTheLoai.php";
+const GetTruyenTheoIdTheLoaiService = UrlBase + "GetTruyenTheoIdTheLoai.php";
+
+export {
+    DangNhapService,
+    GetToanBoTheLoaiService,
+    GetTruyenTheoIdTheLoaiService
+}
